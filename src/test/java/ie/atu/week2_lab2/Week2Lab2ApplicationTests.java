447c2131968369608b0ada784c1193af237b7338
KEELANMURPHY-STUDENT <G00435965@atu.ie>
2025-09-24 14:31:40 +0100

    Setting up springboot project

---
+++ src/test/java/ie/atu/week2_lab2/Week2Lab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week2_lab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week2Lab2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
